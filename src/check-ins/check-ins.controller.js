3e924ae3a39c43022bf984ea92b2487ca29c6d0a
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-12-12 20:22:06 +0100

    Manual deployment

---
+++ src/check-ins/check-ins.controller.js
@@ -0,0 +1,21 @@
+(function(){
+
+  angular
+       .module('checkIns')
+       .controller('CheckInsController', [
+          CheckInsController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function CheckInsController() {
+    var self = this;
+
+  }
+
+})();
