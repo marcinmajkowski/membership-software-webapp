0a37b4fce030d25e33ae2cdbf27f03d90be39d81
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-12-17 19:51:23 +0100

    Manual deploy

--- src/membership-software.config.js
+++ src/membership-software.config.js
@@ -10,14 +10,18 @@
                 .icon("menu", "./assets/svg/menu.svg", 24)
                 .icon("share", "./assets/svg/share.svg", 24)
                 .icon("add", "./assets/svg/ic_add_black_24px.svg", 24)
+                .icon("search_white", "./assets/svg/ic_search_white_24px.svg", 24)
                 .icon("search", "./assets/svg/ic_search_black_24px.svg", 24)
+                .icon("close_white", "./assets/svg/ic_close_white_24px.svg", 24)
                 .icon("mode_edit", "./assets/svg/ic_mode_edit_black_24px.svg", 24)
                 .icon("more_vert", "./assets/svg/more_vert.svg", 24)
                 .icon("delete_forever", "./assets/svg/ic_delete_forever_black_24px.svg", 24)
                 .icon("google_plus", "./assets/svg/google_plus.svg", 512)
                 .icon("hangouts", "./assets/svg/hangouts.svg", 512)
                 .icon("twitter", "./assets/svg/twitter.svg", 512)
-                .icon("phone", "./assets/svg/phone.svg", 512);
+                .icon("phone", "./assets/svg/phone.svg", 512)
+                .icon("person_add", "./assets/svg/ic_person_add_24px.svg", 24)
+                .icon("person_add_black", "./assets/svg/ic_person_add_black_24px.svg", 24);
 
             $mdThemingProvider
                 .definePalette('black', {
